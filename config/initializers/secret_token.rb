ea0f04b6657ce70188e8312d8424be5463c77f60
Ambert Ho <aho@rpxcorp.com>
2012-10-15 19:01:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylestalk::Application.config.secret_token = '98605b7f5c093405b641d7567026f26b5143848594b7e7bba12061721ca970f7bbc1fd1ccb51ff3dadfa8fa416ac6ec23a81c8ee0b0bbfd86e762c79437334b9'
