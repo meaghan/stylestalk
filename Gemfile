9376dda3a87c703b4d07834a1533d8831345a4fc
Ambert Ho <ambert@opzi.com>
2012-10-23 19:20:38 -0700

    added unicorn

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'rails', '3.2.8'
 gem 'pg'
 
 
+gem 'unicorn'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Stylestalk
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    config.logger = Logger.new(STDOUT)
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
